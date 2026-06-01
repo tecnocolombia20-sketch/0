e661c290170bb603c31c126c3a669d63ff05f3fb
tecnocolombia20-sketch <tecnocolombia20@gmail.com>
2026-06-01 11:02:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ window.addEventListener('scroll', function () {
 
 // AGREGA TODAS LAS FOTOS QUE QUIERAS AQUÍ. SIN LÍMITE.
 const galleryImages = [
-  { src: "https://res.cloudinary.com/dsh0z1w5j/image/upload/v1780328990/photo_2026-06-01_10-48-38_e8ahj4.jpg", alt: "Imagen 1" },
+  { src: "https://res.cloudinary.com/dsh0z1w5j/image/upload/v1780329645/file_00000000993c720e95c0c3db9b181dbf_i1uute.png", alt: "Imagen 1" },
   { src: "https://res.cloudinary.com/dsh0z1w5j/image/upload/v1779143770/IMG_20260518_172557_ktrojo.png", alt: "Imagen 2" },
   { src: "https://res.cloudinary.com/dsh0z1w5j/image/upload/v1779143770/IMG_20260518_172331_qc7oyn.png", alt: "Imagen 3" },
   { src: "https://res.cloudinary.com/dsh0z1w5j/image/upload/v1779144794/IMG_20260518_172846_s9chpm.png", alt: "Imagen 4" },
